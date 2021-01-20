4a1350de8897c0c1c7f452b33406ff1c190dabe2
vluhavaya <volhal@uw.edu>
2021-01-20 13:55:54 -0800

    edited

---
+++ js/app.js
@@ -0,0 +1,36 @@
+let myNumber=0.0987;
+console.log(typeof myNumber);
+if (false) 
+{console.log('This is not a number!');}
+
+if (myNumber%3==0)
+{console.log('Gen');}
+
+if(myNumber%5==0)
+{console.log('Buzz');}
+
+if(myNumber%3*5==0)
+{console.log('GenBuzz');}
+
+else {
+    console.log(myNumber);
+}
+
+
+let storeItem = 'shoes';
+ 
+if (storeItem === 'shoes') {
+  console.log('Shoes are $50');
+} else if (storeItem === 'Jeans'){
+  console.log('Jeans are $25');
+} else if (storeItem === 'Hat'){
+    console.log('Hat is $15');
+  } 
+  else if (storeItem === 'Socks'){
+    console.log('Socks are $2');
+  } 
+else {
+  console.log('Invalid item');
+}
+
+Math.floor(Math.random()*50);
